6c6647324e61fb4b82b1f7285522d29636ef01ad
rahul-rky <rahul.rky4@gmail.com>
2023-07-01 17:56:03 +0530

    deploy

--- src/iloveyou.js
+++ src/iloveyou.js
@@ -5,7 +5,7 @@ import React, { createFactory } from 'react';
         <h1 >I love you Rekha</h1>
         <h2 style={{color:"grey"}}>I really do love you</h2>
         <h2 style={{color:"grey"}}>You are here in my Heart Babe</h2>
-        <button><a href='/us'>Look at Us</a></button>
+        <button><a href='usonlyus/us'>Look at Us</a></button>
         </div>
     )
  }
